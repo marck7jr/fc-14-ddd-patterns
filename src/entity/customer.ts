32dd0e1a0890f968c2504f2855ca3c0b4af8b4d2
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2022-07-18 17:39:15 -0400

    Add validate method for customer class

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,22 +1,39 @@
 class Customer {
-    _id: string;
-    _name: string = "";
-    _address: string = "";
-    _active: boolean = true;
+  _id: string;
+  _name: string = "";
+  _address: string = "";
+  _active: boolean = true;
 
-    constructor(id: string) {
-        this._id = id;
+  constructor(id: string, name: string) {
+    this._id = id;
+    this._name = name;
+    this.validate();
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
     }
 
-    changeName(name: string) {
-        this._name = name;
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+  }
+
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  activate() {
+    if (this._address.length === 0) {
+      throw new Error("Address is mandatory to activate a customer");
     }
 
-    activate() {
-        this._active = true;
-    }
+    this._active = true;
+  }
 
-    deactivate() {
-        this._active = false;
-    }
+  deactivate() {
+    this._active = false;
+  }
 }
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,3 @@
 export function hello(name: string): string {
-    return `Hello ${name}`;
+  return `Hello ${name}`;
 }
