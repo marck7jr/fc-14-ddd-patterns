cb227637d59e3cde68b705174553d935d9fa2cac
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2022-07-18 17:30:29 -0400

    Add changeName, activate and deactivate methods for customer class

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,31 +1,22 @@
 class Customer {
     _id: string;
-    _name: string;
-    _address: string;
+    _name: string = "";
+    _address: string = "";
+    _active: boolean = true;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string) {
         this._id = id;
-        this._name = name;
-        this._address = address;
     }
 
-    get id(): string {
-        return this._id;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    get address(): string {
-        return this._address;
-    }
-
-    set name(name: string){
+    changeName(name: string) {
         this._name = name;
     }
 
-    set address(address: string) {
-        this._address = address;
+    activate() {
+        this._active = true;
+    }
+
+    deactivate() {
+        this._active = false;
     }
 }
