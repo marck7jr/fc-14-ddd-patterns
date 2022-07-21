4753a6bbf3ad3fb8242bea63418af4fe62c32101
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2022-07-20 20:22:04 -0400

    Add types validation in test script

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ customer.Address = address;
 customer.activate();
 
 // Order Aggregate
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 20);
+const item1 = new OrderItem("1", "Item 1", 10, "1", 1);
+const item2 = new OrderItem("2", "Item 2", 20, "2", 2);
 const order = new Order("1", "123", [item1, item2]);
